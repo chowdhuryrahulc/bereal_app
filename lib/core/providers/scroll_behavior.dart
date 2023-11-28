e486fdb72b272caebf8cde550b6a1f367deb44b2
Rahul Chowdhury Linux Mint <chowdhuryrahulc@gmail.com>
2023-11-28 13:18:28 +0530

    url behaviour

--- lib/core/providers/scroll_behavior.dart
+++ lib/core/providers/scroll_behavior.dart
@@ -1,7 +1,12 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+part 'scroll_behavior.g.dart';
+
 @riverpod
 class ScrollBehavior extends _$ScrollBehavior {
+  @override
+    return true;
+  }
 
   void enable(bool value) {
     state = value;
